52727c801491104ff5b5ca11113df1db86761a6f
cherry <jian_yuehu@163.com>
2019-10-18 09:55:58 +0800

    amqp usring demonstrated

---
+++ src/main/java/com/cherry/prospring5/ch12/AmqpRpcDemo.java
@@ -0,0 +1,20 @@
+package com.cherry.prospring5.ch12;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class AmqpRpcDemo {
+    private static Logger logger = LoggerFactory.getLogger(AmqpRpcDemo.class);
+
+    public static void main(String... args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:spring/amqp-rpc-app-context.xml");
+        ctx.refresh();
+        WeatherService weatherService = ctx.getBean(WeatherService.class);
+        logger.info("Forecast for FL: " + weatherService.getForecast("FL"));
+        logger.info("Forecast for MA: " + weatherService.getForecast("MA"));
+        logger.info("Forecast for CA: " + weatherService.getForecast("CA"));
+        ctx.close();
+    }
+}
