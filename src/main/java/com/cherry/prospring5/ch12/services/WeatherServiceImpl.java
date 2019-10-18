4345fe9675b3cfd59bb354c848e278006f707f9d
cherry <jian_yuehu@163.com>
2019-10-18 16:16:34 +0800

    rabbitmq javaconfig usring demonstrated

--- src/main/java/com/cherry/prospring5/ch12/services/WeatherServiceImpl.java
+++ src/main/java/com/cherry/prospring5/ch12/services/WeatherServiceImpl.java
@@ -1,9 +1,9 @@
-package com.cherry.prospring5.ch12;
+package com.cherry.prospring5.ch12.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +12,8 @@ public class WeatherServiceImpl {
     private static Logger logger =
             LoggerFactory.getLogger(WeatherServiceImpl.class);
 
-    @RabbitListener(containerFactory = "rabbitListenerContainerFactory",
-            queues = "forecasts")
+    @RabbitListener(queues = "forecasts")
+    @RabbitHandler
     public void getForecast(String stateCode) {
         if ("FL".equals(stateCode)) {
             logger.info("Hot");
@@ -23,4 +23,5 @@ public class WeatherServiceImpl {
             logger.info("Not available at this time");
         }
     }
+
 }
